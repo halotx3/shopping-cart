0bdf2c7f19ed524c9b9cc71dbd8c39ca450b2842
Christopher Brownlee <cdbreach@gmail.com>
2019-02-28 22:49:48 -0500

    Edited onclick function

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -2,14 +2,21 @@ let itemList = [{
 
 }];
 
+let render = function(){
+
+}
+
 let addCart = function(){
     let itemName = $(this)[0].innerText;
     let itemCat = $(this).attr('data-catagory');
-    let itemPrice = $(this).val;
+    let itemPrice = $(this).attr('data-price');
 
     console.log(itemName);
+    console.log(itemList.name);
     console.log(itemCat);
     console.log(itemPrice);
+
+    render();
 }
 
 $('#sector').on('click', '.item', addCart);
